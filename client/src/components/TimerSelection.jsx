300570b974bf1beada66a287c62404d988b77cd4
Lucie Wang <lucie.wangxy@gmail.com>
2020-03-25 14:28:14 -0700

    Images width definition

--- client/src/components/TimerSelection.jsx
+++ client/src/components/TimerSelection.jsx
@@ -9,19 +9,19 @@ const TimerSelection = () => {
         <div>
             <div>
             <label>20 mins</label>
-            <img src={Timer20} alt="20-min-Timer"/>
+            <img src={Timer20} alt="20-min-Timer" width="30%"/>
             <Link to='/timer/timer20'><button>Go</button></Link> 
             </div>
             
             <div>
             <label>50 mins</label>
-            <img src={Timer50} alt="50-min-Timer"/>
+            <img src={Timer50} alt="50-min-Timer" width="30%"/>
             <Link to='/timer/timer50'><button>Go</button></Link> 
             </div>
             <div>
             <form>
             <label>Custom</label>
-            <img src={TimerCustom} alt="20-min-Timer"/>
+            <img src={TimerCustom} alt="20-min-Timer" width="30%"/>
             <label>Hours</label>
             <input name="hours" type="number" step="1" min="0"/>
             <label>Minutes</label>
