11b1e208dc3f0bba1e5e8084b1f0231cb9d2806f
Greg Chance <gchance@gregsMac.local>
2020-03-24 21:46:02 -0700

    created nav component with css

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,18 @@ import {Router} from '@reach/router';
 import Wrapper from './components/Wrapper';
 import Success from './components/Success';
 import axios from 'axios'
-import StuffAndThings from './components/StuffAndThings';
+import NavBar from './components/NavBar';
 
 // i used a wrapper so that you can see the registration and login as different components
 // instead of on one page. the wrapper allows me to put both components on the same route.
 // if the cookie was set with HttpOnly:false then you can show it using {document.cookie}
 function App() {
   return (
-    <div className="App">
-      <StuffAndThings></StuffAndThings>
+    <div className="wrapper">
       <Router>
         <Wrapper path="/"/>
         <Success path="/success" />
+        <NavBar path="/home"></NavBar>
       </Router> 
     </div>
   );
