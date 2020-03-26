b94df3fccc10bc585fc58bc8d63f28da664f78c1
Lucie Wang <lucie.wangxy@gmail.com>
2020-03-26 15:56:24 -0700

    completed Profile Page with displaying, editing

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import profileCard from '../images/profileCard.png'
+import profileCard from '../images/profileCard.png';
+import editPad from '../images/editPad.jpg';
 
 
 const Profile = (props) => {
@@ -23,8 +24,8 @@ const Profile = (props) => {
         password: ''
     })
 
-    const [update, setUpdate] = useState(false)
-    const [hide, setHide] = useState(false)
+    const [errorState, setErrorState] = useState([])
+    const [hide, setHide] = useState(true)
     useEffect(
         () => {
             axios
@@ -38,23 +39,93 @@ const Profile = (props) => {
         },
         []
     );
+
+    const Edit=(e)=>{
+        setHide(!hide)
+    }
+
+    const CloseEdit=(e)=>{
+        setHide(true)
+    }
+
+    const onSH = (e) => {
+        e.preventDefault();
+        axios.put(`http://localhost:8000/api/v1/updateOne/${userID}`, state,{withCredentials:true})
+        .then(response => {
+            if(response.data.errors){
+                const temp = []
+                for(let key in response.data.errors) {
+                    temp.push(response.data.errors[key].message)
+                }
+                setErrorState(temp)
+            } else {
+                console.log(state);
+                setHide(true)
+            }
+        })
+        .catch(error => console.log(error))
+    }
+    const onCH = (e) => {
+        setState({
+            ...state,
+            [e.target.name]:e.target.value
+        })
+    }
+
     return (
-        <div>
+        <div className='profileContainer'>
             <div className='badge'>
                 <div className='profileBg'>
-                    <img src={profileCard} alt="profile" width='20%' />
+                    <img src={profileCard} alt="profile" width='100%' />
                     <div className='profileInfo'>
-                        <form className='form'>
-                            <img className='profileImg' src={state.profileImage} alt='No Profile Image'/>
+                        <img className='profileImg' src={state.profileImage} alt='No Profile Image'/>
+                            <div className='profileLabel'>
                             <p>First Name</p>
                             <input type="text" name="firstName" value={state.firstName} />
                             <p>Last Name</p>
                             <input type="text" name="lastName" value={state.lastName} />
                             <p>Email</p>
                             <input type="email" name="email" value={state.email} />
-                        </form>
+                            </div>
+                            <div className='buttons'>
+                            <button hidden={!hide}onClick={Edit}>Edit</button>
+                            <button hidden={hide}onClick={CloseEdit}>Close Edit</button>
+                            </div>
                     </div>
                 </div>
+            </div>
+            <div hidden={hide}>
+            <div className='editColumn'>
+            <div className="editWrapper">
+            <img src={editPad} alt="tags" width="92%"/>
+            <div className="editProfile">
+            {errorState.map((item, index) => (
+                <p key={index}>{item}</p>
+            ))}
+            <form onSubmit={onSH}>
+                <div className='imageTag'>
+                <img src={state.profileImage} alt="Profile Image" width='25%'/><br/>
+                <input type="text" name="profileImage" onChange={onCH} value={state.profileImage}/>
+                </div>
+                <div className='labelTag'>
+                <label>First Name </label>
+                <input type="text" name="firstName" onChange={onCH} value={state.firstName}/>
+                </div>
+                <div className='labelTag'>
+                <label>Last Name </label>
+                <input type="text" name="lastName" onChange={onCH} value={state.lastName}/>
+                </div>
+                <div className='labelTag'>
+                <label>Email </label>
+                <input type="email" name="email" onChange={onCH} value={state.email}/>
+                </div>
+                <button type="submit">Save</button>
+
+            </form>
+            </div>
+            </div>
+
+        </div>
             </div>
         </div>
     )
